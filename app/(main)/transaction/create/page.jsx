c417928fe0e71be391d0eb490d5b604eefd44ae3
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-06-25 12:35:19 +0530

    created new underconstruction page and added that to add transaction route it helps us to render that page insted of getting runtime error becouse we still not added that add transaction page

---
+++ app/(main)/transaction/create/page.jsx
@@ -0,0 +1,11 @@
+import UnderConstructionPage from '@/app/underConstuction-page'
+import React from 'react'
+
+
+const CreateTransaction = () => {
+  return(
+    <UnderConstructionPage />   
+  )
+}
+
+export default CreateTransaction
---
+++ app/underConstuction-page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+const UnderConstructionPage = () => {
+  return (
+     <div className='flex flex-col items-center justify-center'>
+        <div className=" flex flex-col items-center justify-center bg-white rounded-xl shadow-lg p-8 max-w-md w-full">
+        <p className="text-center text-2xl font-semibold text-gray-800 mb-2">
+          🚧 Under Construction
+        </p>
+        <p className="text-center text-gray-600 mb-6">
+          This page is being built.<br />
+          <span className="text-base text-gray-500">It will be ready soon!</span>
+        </p>
+        <Link href="/" passHref>
+          <Button className="w-full">Back to Home</Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default UnderConstructionPage
