33fa924ff9c96ed2fea70529a58875d6396cbb5f
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-06-28 21:56:34 +0530

    added rechrts pakage and created and wonderfull barg graph to analyze the transactions each day with filtering options in it and some improvements

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -3,6 +3,7 @@ import { notFound } from 'next/navigation'
 import React, { Suspense } from 'react'
 import TransactionTable from '../_components/transaction-table';
 import { BarLoader } from 'react-spinners';
+import { AccountChart } from '../_components/account-chart';
 
 const AccountsPage = async ({ params }) => {
   const resolvedParams = await params;
@@ -30,6 +31,9 @@ const AccountsPage = async ({ params }) => {
         </div>
 
         {/* <div>Chart section</div> */}
+        <Suspense fallback = {<BarLoader className='mt-4' width={"100%"} color='#9333ea'/>}>
+ <AccountChart transactions={transactions}/>
+        </Suspense>
 
         <Suspense fallback = {<BarLoader className='mt-4' width={"100%"} color='#9333ea'/>}>
  <TransactionTable transactions={transactions}/>
