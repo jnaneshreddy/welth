bdf075ad43b70aa72c6c1fd7b7b25946596b8ead
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-06-30 14:30:15 +0530

    worked on cron job and budget bar in home screen and tested automation budget traking using inggest

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,30 @@ import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import { getUserAccount } from '@/actions/dashboard'
 import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from '@/actions/budget'
+import BudgetProgress from './_components/budget-progress'
 
 async function DashboardPage ()  {
   const accounts = await getUserAccount();
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+    // to fetch the budget of default account 
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
+
+
   return (
-    <div className='px-5'>
-      <div>Budeget Progress</div>
+    <div className='space-y-8'>
+      {/* <div>Budeget Progress</div> */}
+
+      {defaultAccount && (<BudgetProgress 
+      initialBudget = {budgetData?.budget} 
+      currentExpenses = {budgetData?.currentExpenses || 0} 
+      />
+      )}
+
       <div>Dashboard Overviwe</div>
       {/* <div>Accounts Grid</div> */}
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
