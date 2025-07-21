a26e186e7df638f026da4ca898ca5b238dea625b
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-07-21 21:24:29 +0530

    added recent transactions on dashboard properly

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import CreateAccountDrawer from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
-import { getUserAccount } from '@/actions/dashboard'
+import { getDashboardData, getUserAccount } from '@/actions/dashboard'
 import AccountCard from "./_components/account-card";
 import { getCurrentBudget } from '@/actions/budget'
 import {BudgetProgress} from './_components/budget-progress'
+import DashboardOverview from './_components/transaction-overview'
 
 async function DashboardPage ()  {
   const accounts = await getUserAccount();
@@ -17,6 +18,7 @@ async function DashboardPage ()  {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
 
+  const transactions  = await getDashboardData()
 
   return (
     <div className='space-y-8'>
@@ -28,19 +30,24 @@ async function DashboardPage ()  {
       />
       )}
 
-      <div>Dashboard Overviwe</div>
+      {/* <div>Dashboard Overviwe</div> */}
+      <Suspense fallback={"Loading Overview......"}>
+            <DashboardOverview
+            accounts={accounts || []}
+            transactions = {transactions || []}
+            />
+      </Suspense>
       {/* <div>Accounts Grid</div> */}
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
         <CreateAccountDrawer>
-          <Card className="hover:shadow-md transition-shodow curser-pointer border-dashed">
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
             <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5 ">
               <Plus className="h-10 w-10 mb-2" />
               <p className='text-sm font-medium'>Add New Account</p>
             </CardContent>
           </Card>
         </CreateAccountDrawer>
-        {accounts.length>0 && accounts?.map((account)=>{
-
+        {accounts && accounts.length > 0 && accounts.map((account) => {
           return <AccountCard key={account.id} account={account} />
         })}
       </div>
