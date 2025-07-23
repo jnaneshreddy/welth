ae466d5fd7aa5cec71724f01931445885eed669e
MR.JNANESH REDDY <71424573+jnaneshreddy@users.noreply.github.com>
2025-07-23 23:44:05 +0530

    Update recipt-scanner.jsx
    In added choose file option

--- app/(main)/transaction/_components/recipt-scanner.jsx
+++ app/(main)/transaction/_components/recipt-scanner.jsx
@@ -1,67 +1,70 @@
-"use client"
+"use client";
 
 import { scanRecipt } from '@/actions/transaction';
 import { Button } from '@/components/ui/button';
 import useFetch from '@/hooks/use-fetch';
-import { Camera,Loader2 } from 'lucide-react';
-import React, { useEffect, useRef } from 'react'
+import { Camera, Loader2 } from 'lucide-react';
+import React, { useEffect, useRef } from 'react';
 import { toast } from 'sonner';
 
-const ReciptScanner = ({onScanComplete}) => {
-    const fileInputRef = useRef();
+const ReciptScanner = ({ onScanComplete }) => {
+  const fileInputRef = useRef();
 
-    const {
-        loading:scanReciptLoading,
-        fn:scanReciptFn,
-        data:scannedData,
-    } = useFetch(scanRecipt)
+  const {
+    loading: scanReciptLoading,
+    fn: scanReciptFn,
+    data: scannedData,
+  } = useFetch(scanRecipt);
 
-    const handleReciptScan = async (file) =>{
-        if(file.size > 10*1024*1024){
-            toast.error("File size Should be less then 10Mb");
-            return;
-        }
-
-        await scanReciptFn(file)
+  const handleReciptScan = async (file) => {
+    if (file.size > 10 * 1024 * 1024) {
+      toast.error("File size should be less than 10MB");
+      return;
     }
+    await scanReciptFn(file);
+  };
 
-    useEffect(()=>{
-        if (scannedData && !scanReciptLoading){
-            onScanComplete(scannedData);
-            toast.success("Recipt Scanned succesfully");
-        }
-    },[scanReciptLoading,scannedData]);
-
-    
+  useEffect(() => {
+    if (scannedData && !scanReciptLoading) {
+      onScanComplete(scannedData);
+      toast.success("Receipt scanned successfully");
+    }
+  }, [scanReciptLoading, scannedData]);
 
   return (
     <div>
-        <input type="file" ref={fileInputRef}
-        className='hidden'
-        accept='image/*'
-        capture="environment"
-        onChange={(e)=>{
-            const file = e.target.files?.[0];
-            if(file) handleReciptScan(file);
+      <input
+        type="file"
+        ref={fileInputRef}
+        className="hidden"
+        accept="image/*"
+        onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (file) handleReciptScan(file);
         }}
-        />
-        <Button
+      />
+      <Button
         type="button"
         variant="outline"
         className="w-full h-10 bg-gradient-to-br from-orange-500 via-pink-500 to-purple-500 animate-gradient hover:opacity-90 
         transition-opacity text-white hover:text-white"
-        onClick={()=> fileInputRef.current?.click()}
+        onClick={() => fileInputRef.current?.click()}
         disabled={scanReciptLoading}
-        >{scanReciptLoading ? (<>
-        {""}
-        <Loader2 className='me-2 animate-spin'/>
-        <span>Scanning Recipt...</span>
-        </>):(<>
-        <Camera className="mr-2"/>
-        <span>Scan Recipt With Ai</span>
-        </>)}</Button>
+      >
+        {scanReciptLoading ? (
+          <>
+            <Loader2 className="me-2 animate-spin" />
+            <span>Scanning Receipt...</span>
+          </>
+        ) : (
+          <>
+            <Camera className="mr-2" />
+            <span>Scan Receipt with AI</span>
+          </>
+        )}
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default ReciptScanner
+export default ReciptScanner;
